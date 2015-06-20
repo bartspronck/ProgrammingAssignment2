41dde6c35918aa41bdfd989567e77711e03c84cb
Bart Spronck <b.spronck@maastrichtuniversity.nl>
2015-06-20 13:54:20 +0200

    Added some extra annotation to the solution.

--- cachematrix.R
+++ cachematrix.R
@@ -14,14 +14,14 @@
 ## calculated.
 
 makeCacheMatrix <- function(x = matrix()) {
-  inv <- NULL          #The inverse is not yet calculated
-  set <- function(y) { #Function for setting the matrix
+  inv <- NULL          #The inverse is not yet calculated.
+  set <- function(y) { #Function for setting the matrix.
     M <<- y
-    inv <<- NULL       #If a new matrix is set, remove a (previously calculated) inverse
+    inv <<- NULL       #If a new matrix is set, remove a (previously calculated) inverse.
   }
-  get <- function() M
-  setInverse <- function(inverse) inv <<- inverse
-  getInverse <- function() inv
+  get <- function() M  #Function for getting the matrix.
+  setInverse <- function(inverse) inv <<- inverse  #Function for setting the calculated inverse.
+  getInverse <- function() inv                     #Function for getting the stored inverse.
   list(set = set, 
        get = get,
        setInverse = setInverse,
@@ -32,16 +32,18 @@ makeCacheMatrix <- function(x = matrix()) {
 ## cacheSolve(x, ...) performs matrix inversion for a matrix stored in an object x created
 ## by makeCacheMatrix. It takes optional arguments which are passed through to R's solve
 ## function.
-## cacheSolve returns the inverted matrix.
+## cacheSolve returns the inverted matrix, and stores the inverted matrix in x.
 
 cacheSolve <- function(x, ...) {
   inv <- x$getInverse() #Try to get a (previously calculated) inverse.
   if(!is.null(inv)) {
     message("Getting previously calculated inverse...")
-    return(inv)
+    return(inv) #Return the previously calculated inverse.
   }
-  M <- x$get()
-  inv <- solve(M, ...)
-  x$setInverse(inv)
-  inv
+  
+  #If the inverse was not yet calculated...
+  M <- x$get()         #...get the matrix to be inverted.
+  inv <- solve(M, ...) #Compute the inversion.
+  x$setInverse(inv)    #Set the inversion result to the object x.
+  inv                  #Return the inversion result.
 }
